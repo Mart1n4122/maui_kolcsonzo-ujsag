44bcb2117dab9415110c361f9643c34f929799b1
Tóthmihály Martin <b1totmar@vasvari.org>
2025-11-12 09:28:44 +0100

    nemtom

--- vizibicikli/Program.cs
+++ vizibicikli/Program.cs
@@ -1,6 +1,8 @@
 ﻿using vizibicikli;
 
-var lines = File.ReadAllLines("vizibicikli.csv").Skip(1).Where(l => !string.IsNullOrWhiteSpace(l));
+var lines = File.ReadAllLines("vizibicikli.csv")
+    .Skip(1)
+    .Where(l => !string.IsNullOrWhiteSpace(l));
 var rents = new List<Rent>();
 
 foreach (var line in lines)
@@ -24,11 +26,46 @@ foreach (var rent in rents)
     Console.WriteLine($"{rent.nev} {rent.azonosito} {rent.kora} {rent.kperc} {rent.vora} {rent.vperc}");
 }
 
-foreach (var rent in rents)
-{
-    var kezdes = new TimeOnly(rent.kora, rent.kperc);
-    var vegzes = new TimeOnly(rent.vora, rent.vperc);
+const int pricePerBlock = 1000;
+const int minutesPerBlock = 30;
 
-    var eltelt = vegzes - kezdes;
-    
+var groups = rents.GroupBy(r => r.azonosito).OrderBy(g => g.Key);
+
+Console.WriteLine();
+Console.WriteLine("Összesítés csoportonként:");
+
+foreach (var group in groups)
+{
+    TimeSpan groupTotalDuration = TimeSpan.Zero;
+    int groupTotalFee = 0;
+    int entries = 0;
+
+    foreach (var rent in group)
+    {
+        var kezdes = new TimeOnly(rent.kora, rent.kperc);
+        var vegzes = new TimeOnly(rent.vora, rent.vperc);
+
+        if (vegzes < kezdes)
+        {
+            vegzes = vegzes.AddHours(24);
+        }
+
+        var eltelt = vegzes - kezdes;
+        groupTotalDuration += eltelt;
+
+        var blocks = (int)Math.Ceiling(eltelt.TotalMinutes / minutesPerBlock);
+        var fee = blocks * pricePerBlock;
+        groupTotalFee += fee;
+
+        entries++;
+    }
+
+    var hours = (int)groupTotalDuration.TotalHours;
+    var minutes = groupTotalDuration.Minutes;
+
+    Console.WriteLine();
+    Console.WriteLine($"Csoport: {group.Key}");
+    Console.WriteLine($"  Bejegyzések száma: {entries}");
+    Console.WriteLine($"  Összes idő: {hours} óra {minutes} perc");
+    Console.WriteLine($"  Összes fizetendő: {groupTotalFee} Ft");
 }
