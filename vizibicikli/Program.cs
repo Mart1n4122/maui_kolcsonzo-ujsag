3f8e858992087d6fc5276e54fb0f527e0fb4a9b0
Tóthmihály Martin <b1totmar@vasvari.org>
2025-11-12 08:55:10 +0100

    otletem sincs rola

---
+++ vizibicikli/Program.cs
@@ -0,0 +1,34 @@
+﻿using vizibicikli;
+
+var lines = File.ReadAllLines("vizibicikli.csv").Skip(1).Where(l => !string.IsNullOrWhiteSpace(l));
+var rents = new List<Rent>();
+
+foreach (var line in lines)
+{
+    var columns = line.Split(',').Select(s => s.Trim()).ToArray();
+    var rent = new Rent
+    {
+        nev = columns[0],
+        azonosito = columns[1],
+        kora = int.Parse(columns[2]),
+        kperc = int.Parse(columns[3]),
+        vora = int.Parse(columns[4]),
+        vperc = int.Parse(columns[5])
+    };
+    rents.Add(rent);
+}
+
+Console.WriteLine("CSV adat:");
+foreach (var rent in rents)
+{
+    Console.WriteLine($"{rent.nev} {rent.azonosito} {rent.kora} {rent.kperc} {rent.vora} {rent.vperc}");
+}
+
+foreach (var rent in rents)
+{
+    var kezdes = new TimeOnly(rent.kora, rent.kperc);
+    var vegzes = new TimeOnly(rent.vora, rent.vperc);
+
+    var eltelt = vegzes - kezdes;
+    
+}
---
+++ vizibicikli/Rent.cs
@@ -0,0 +1,12 @@
+﻿namespace vizibicikli
+{
+    public class Rent
+    {
+       public string nev { get; set; }
+        public string azonosito { get; set; }
+        public int kora { get; set; }
+        public int kperc { get; set; }
+        public int vora { get; set; }
+        public int vperc { get; set; }
+    }
+}
