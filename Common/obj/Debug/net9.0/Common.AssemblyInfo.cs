025105e8309a7af312ce4047e6b87e55c03dbe21
Eperjesi Vencel <b1epeven@vasvari.org>
2025-12-03 08:09:53 +0100

    valami

--- Common/obj/Debug/net9.0/Common.AssemblyInfo.cs
+++ Common/obj/Debug/net9.0/Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80316a343a7d09c7ae31f2b9334df3544b983192")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+970144c15b35bbc4801252d2f24af5e00de43bc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
