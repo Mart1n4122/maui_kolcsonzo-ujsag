c042a097fab400cbedf1dd27a50e8e25ac7fe905
Eperjesi Vencel <b1epeven@vasvari.org>
2025-12-03 08:45:17 +0100

    adawafda

--- Service/IBlogService.cs
+++ Service/IBlogService.cs
@@ -1,18 +1,12 @@
 ﻿using Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Services
+namespace Services;
+
+public interface IBlogService
 {
-    public interface IBlogService
-    {
-        Task<NewspaperDto> GetAsync(int id);
-        Task<NewspaperDto> ListAllAsync();
-        Task CreateAsync(NewspaperDto dto);
-        Task UpdateAsync(NewspaperDto dto);
-        Task DeleteAsync(int id);
-    }
+    Task<NewspaperDto> GetAsync(int id);
+    Task<List<NewspaperDto>> ListAllAsync();
+    Task CreateAsync(NewspaperDto dto);
+    Task UpdateAsync(NewspaperDto dto);
+    Task DeleteAsync(int id);
 }
